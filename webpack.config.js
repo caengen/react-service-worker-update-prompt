cae1edf761172298d3a77be0bfc55e72055b1658
Christian Engen <chris.a.engen@gmail.com>
2019-10-22 10:51:18 +0200

    add rollup config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const webpack = require("webpack");
+const pkg = require("./package.json");
 const path = require("path");
 
+const libraryName = pkg.name;
+
 const config = {
-  entry: ["./src/index.js"],
+  entry: path.join(__dirname, "./src/index.js"),
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    path: path.join(__dirname, "./build"),
+    filename: "bundle.js",
+    library: libraryName,
+    libraryTarget: "umd",
+    publicPath: '/build/',
+    umdNamedDefine: true
+  },
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
   },
   module: {
     rules: [
@@ -16,6 +28,7 @@ const config = {
       {
         test: /\.(js|jsx)$/,
         use: "babel-loader",
+        include: path.resolve(__dirname, "src"),
         exclude: /node_modules/
       },
       {
@@ -25,8 +38,26 @@ const config = {
     ]
   },
   resolve: {
-    extensions: [".js", ".jsx"]
-  }
+    extensions: [".js", ".jsx"],
+    alias: {          
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+    }  
+  },
+  externals: {      
+    react: {          
+        commonjs: "react",          
+        commonjs2: "react",          
+        amd: "React",          
+        root: "React"      
+    },      
+    "react-dom": {          
+        commonjs: "react-dom",          
+        commonjs2: "react-dom",          
+        amd: "ReactDOM",          
+        root: "ReactDOM"      
+    }  
+} 
 };
 
 module.exports = config;
